3bf714932d8ddfaefe2aadb28bc0b1c651bc0094
Andy <76755871+royandi200@users.noreply.github.com>
2026-04-20 22:17:14 -0500

    feat(dispatch): add frontend API method for dispatch history

--- src/api/dispatch.api.js
+++ src/api/dispatch.api.js
@@ -1,3 +1,4 @@
 import client from './client'
 
 export const createDispatch = (body) => client.post('/dispatch', body)
+export const listDispatches = (params = {}) => client.get('/dispatch', { params })
